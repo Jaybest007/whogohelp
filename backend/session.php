8e93296e4e728911ba73c1f4161d51c5fbfec2a3
Jaybest007 <104288328+Jaybest007@users.noreply.github.com>
2025-05-24 11:15:16 -0700

    Create session.php

---
+++ backend/session.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+if (isset($_SESSION['user'])){
+    echo json_encode([
+        'loggedIn' => true,
+        'user' => $_SESSION['user']
+    ]);
+
+}else{
+    echo json_encode([
+        'loggedIn' => false,
+        'user' => null
+    ]);
+}
