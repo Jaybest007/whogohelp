63217c1c53b5becbdd9d64feb16445dfb94e707c
Jaybest007 <104288328+Jaybest007@users.noreply.github.com>
2025-05-24 11:24:02 -0700

    Update signup.php

--- backend/signup.php
+++ backend/signup.php
@@ -1,6 +1,5 @@
 <?php 
-
-header("Access-control-Allow-Origin: https://whogohelp-backend.onrender.com");
+header("Access-control-Allow-Origin: *");
 header("Acess-Control-Allow-Method: GET, POST, OPTIONS ");
 header("Access-Control-Allow:Headers: content-type");
 header("content-Type: application/json");
