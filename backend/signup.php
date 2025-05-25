07a42b55b0140685f5663941413c0caab406f1a9
Jaybest007 <104288328+Jaybest007@users.noreply.github.com>
2025-05-25 04:13:02 +0000

    update

--- backend/signup.php
+++ backend/signup.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: https://ideal-acorn-vj94vv9gr4pfwxvw-5173.app.github.dev");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
