249b9e08fc333bd9e50f0aadfb7e606f939c36d8
Jaybest007 <104288328+Jaybest007@users.noreply.github.com>
2025-05-28 04:06:08 +0000

    using react lazy() now

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-import Landing from './pages/Landing';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Navbar from './components/Navbar';
-import WithNavbar from './layouts/WithNavbar';
-import Profile from './pages/Profile';
-import BrowseErrandsPage from './pages/BrowseErrandPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+
+const Landing = lazy(() => import('./pages/Landing'));
+const Signup = lazy(() => import('./pages/Signup'));
+const Login = lazy(() => import('./pages/Login'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const WithNavbar = lazy(() => import('./layouts/WithNavbar'));
+const Profile = lazy(() => import('./pages/Profile'));
+const BrowseErrandsPage = lazy(() => import('./pages/BrowseErrandPage'));
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                {/* Routes with Navbar */}
-                <Route element={<WithNavbar />}>
-                    <Route path='/' element={<Landing />} />
-                    <Route path='/dashboard' element={<Dashboard />} />
-                    <Route path='/profile' element={<Profile />} />
-                    <Route path='/errands' element={<BrowseErrandsPage />} />
-                    {/* Add more routes here that need the navbar */}
-                </Route>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    {/* Routes with Navbar */}
+                    <Route element={<WithNavbar />}>
+                        <Route path='/' element={<Landing />} />
+                        <Route path='/dashboard' element={<Dashboard />} />
+                        <Route path='/profile' element={<Profile />} />
+                        <Route path='/errands' element={<BrowseErrandsPage />} />
+                        {/* Add more routes here that need the navbar */}
+                    </Route>
 
-                {/* Pages with no navbar */}
-                <Route path='/signup' element={<Signup />} />
-                <Route path='/login' element={<Login />} />
-            </Routes>
+                    {/* Pages with no navbar */}
+                    <Route path='/signup' element={<Signup />} />
+                    <Route path='/login' element={<Login />} />
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     );
 }
